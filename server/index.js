63030b1f2b8b93b57f9015cdd918e74f0cdf29bc
nqsullivan <100096567+nqsullivan@users.noreply.github.com>
2022-03-02 17:45:31 -0700

    Deployment implementation

--- server/index.js
+++ server/index.js
@@ -17,13 +17,15 @@ app.use(cors());
 
 
 app.use('/team', teamRoutes);
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const CONNECTION_URL = process.env.CONNECTION_URL;
 
 mongoose.connect(CONNECTION_URL)
     .then(() => app.listen(5000, () => console.log(`Server running on port: http://localhost:${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
 
-
-export default CONNECTION_URL;
+mongoose.set('useFindAndModify', false);
