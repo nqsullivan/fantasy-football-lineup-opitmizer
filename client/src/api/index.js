c94909ec1c53d1a5f1a2552119e4aef5b8f5a538
nqsullivan <100096567+nqsullivan@users.noreply.github.com>
2022-03-02 18:19:24 -0700

    Revert "Adding procfile to client"
    This reverts commit fe704fa6cbd49341499c57490ef696c079a069ad.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-//const url = 'http://localhost:5000/team';
-const url = 'https://fantasy-football-100.herokuapp.com/'
+const url = 'http://localhost:5000/team';
 
 export const fetchTeam = () => axios.get(url);
 export const createPlayer = (newPlayer) => axios.post(url, newPlayer);
