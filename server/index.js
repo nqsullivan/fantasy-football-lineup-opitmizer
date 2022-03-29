1b9f980798334269761f38d58039a07c6c4695e2
nqsullivan <100096567+nqsullivan@users.noreply.github.com>
2022-03-28 18:07:09 -0700

    Fixing dependencies

--- server/index.js
+++ server/index.js
@@ -24,6 +24,3 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
-
-
-mongoose.set('useFindAndModify', false);
